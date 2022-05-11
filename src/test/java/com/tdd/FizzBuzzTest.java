f862454ad2634a3e439c7dcbbdee8598c303a407
AlexeyZeynalov <zeynalov.alexey@gmail.com>
2022-05-11 14:47:52 +0300

    add method testForDivisibleByThreeAndFive and testForNotDivisibleByThreeOrFive

--- src/test/java/com/tdd/FizzBuzzTest.java
+++ src/test/java/com/tdd/FizzBuzzTest.java
@@ -32,4 +32,26 @@ class FizzBuzzTest {
         assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
 
     }
+
+    @Test
+    @DisplayName("Divisible by three and five")
+    @Order(3)
+    void testForDivisibleByThreeAndFive() {
+
+        String expected = "FizzBuzz";
+
+        assertEquals(expected, FizzBuzz.compute(15), "Should return FizzBuzz");
+
+    }
+
+    @Test
+    @DisplayName("Not Divisible by three or five")
+    @Order(4)
+    void testForNotDivisibleByThreeOrFive() {
+
+        String expected = "1";
+
+        assertEquals(expected, FizzBuzz.compute(1), "Should return 1");
+
+    }
 }
