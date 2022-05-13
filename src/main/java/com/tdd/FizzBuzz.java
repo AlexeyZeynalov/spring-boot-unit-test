39d7c290074edb771a115a3aefce172633dc806f
AlexeyZeynalov <zeynalov.alexey@gmail.com>
2022-05-13 15:41:21 +0300

    add alternative method compute to class FizzBuzz

--- src/main/java/com/tdd/FizzBuzz.java
+++ src/main/java/com/tdd/FizzBuzz.java
@@ -9,6 +9,25 @@ public class FizzBuzz {
 
     public static String compute(int i) {
 
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if (result.isEmpty()) {
+            result.append(i);
+        }
+
+        return result.toString();
+    }
+
+    /*public static String compute(int i) {
+
         if ((i % 3 == 0) && (i % 5 == 0)) {
             return "FizzBuzz";
         } else if (i % 3 == 0) {
@@ -18,5 +37,5 @@ public class FizzBuzz {
         } else {
             return Integer.toString(i);
         }
-    }
+    }*/
 }
